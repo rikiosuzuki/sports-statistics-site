bee7de1c30b244881507575880d600f9ca7b3fff
rikiosuzuki <suzuki.r@northeastern.edu>
2023-12-05 22:07:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {HashRouter, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+        <HashRouter>
+            <Routes>
+
+            </Routes>
+        </HashRouter>
+    </div>
+  );
+}
+
+export default App;
