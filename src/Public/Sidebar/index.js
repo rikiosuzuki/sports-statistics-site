e8fea95b0edcda07ddecc877713b1e6bbd2faedb
Chris Le <le.chris@northeastern.edu>
2023-12-10 22:49:31 -0500

    fix sidebar

--- src/Public/Sidebar/index.js
+++ src/Public/Sidebar/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./sidebar.css";
+import {
+  FaHome,
+  FaRegNewspaper,
+  FaUsers,
+  FaUser,
+  FaMedal,
+  FaHistory,
+  FaQuestionCircle,
+} from "react-icons/fa";
+import { MdOutlineScoreboard } from "react-icons/md";
 
 function Sidebar() {
   const content = [
-    { name: "Home"},
+    { name: "Home"} ,
     { name: "Games"},
     { name: "News"},
     { name: "Teams"},
@@ -13,6 +23,18 @@ function Sidebar() {
     { name: "History"},
     { name: "About"},
   ];
+
+  const linksToIconsMap = {
+    Home: <FaHome className="text-muted" />,
+    Games: <MdOutlineScoreboard className="text-muted" />,
+    News: <FaRegNewspaper className="text-muted" />,
+    Teams: <FaUsers className="text-muted" />,
+    Players: <FaUser className="text-muted" />,
+    Rankings: <FaMedal className="text-muted" />,
+    History: <FaHistory className="text-muted" />,
+    About: <FaQuestionCircle className="text-muted" />,
+  };
+
   const { pathname } = useLocation();
 
   return (
@@ -29,12 +51,8 @@ function Sidebar() {
               pathname.includes(link.name) && "active"
             }`}
           >
-            <span
-              className={"nav-link-style"}
-              style={{ textDecoration: "none" }}
-            >
+              {linksToIconsMap[link.name]}
               {link.name}
-            </span>
           </Link>
         ))}
       </div>
