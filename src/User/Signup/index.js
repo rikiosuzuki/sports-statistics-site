f3a38130dd2772d3977713206ed7dd60a93d1e1f
rikiosuzuki <suzuki.r@northeastern.edu>
2023-12-12 19:59:10 -0500

    updated sign up

--- src/User/Signup/index.js
+++ src/User/Signup/index.js
@@ -24,16 +24,52 @@ function Signup(){
         <div>
             <h1>Signup</h1>
             {error && <div>{error}</div>}
+            <label>Username</label>
             <input
                 value={credentials.username}
                 onChange={(e) => setCredentials({
                     ...credentials,
                     username: e.target.value })} />
+            <label>Password</label>
             <input
                 value={credentials.password}
                 onChange={(e) => setCredentials({
                     ...credentials,
                     password: e.target.value })} />
+            <label>First Name</label>
+            <input
+                value={credentials.firstName}
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    firstName: e.target.value })} />
+            <label>Last Name</label>
+            <input
+                value={credentials.lastName}
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    lastName: e.target.value })} />
+            <label>Date of Birth</label>
+            <input
+                value={credentials.dob}
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    dob: e.target.value })} />
+            <label>Email</label>
+            <input
+                value={credentials.email}
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    email: e.target.value })} />
+
+            <select onChange={(e) => setCredentials({ ...credentials,
+                role: e.target.value })}>
+                <option value="USER">User</option>
+                <option value="ADMIN">Admin</option>
+                <option value="FACULTY">Faculty</option>
+                <option value="STUDENT">Student</option>
+            </select>
+
+
             <button onClick={signup}>
                 Signup
             </button>
