c6fe9eb4d757a39934b84a7b39f95ba281da6a85
rikiosuzuki <suzuki.r@northeastern.edu>
2023-12-15 13:02:57 -0500

    changes to env var

--- src/User/client.js
+++ src/User/client.js
@@ -5,7 +5,7 @@ const request = axios.create({
 });
 
 
-export const BASE_API =  "http://localhost:4000";
+export const BASE_API =  process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
 
 
