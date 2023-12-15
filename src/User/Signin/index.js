e7c611bc4afcd143ae3fe0f53a52b376fbc423e8
Renke <renkecui@gmail.com>
2023-12-15 17:57:29 -0500

    signin styling

--- src/User/Signin/index.js
+++ src/User/Signin/index.js
@@ -1,29 +1,65 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import * as client from "../client";
-import {useNavigate} from "react-router";
-
+import { useNavigate } from "react-router";
+import "./index.css";
 function Signin() {
-    const [credentials, setCredentials] = useState({ username: "", password: "" });
-    const navigate = useNavigate();
-    const signin = async () => {
-      await client.signin(credentials);
-      navigate("/Profile/User/Activity");
-    };
-    const cancel = async () => {
-        navigate("/Public/Home");
-        };
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const signin = async () => {
+    await client.signin(credentials);
+    navigate("/Profile/User/Activity");
+  };
+  const cancel = async () => {
+    navigate("/Public/Home");
+  };
 
-    return (
-        <div>
-            <h3>Signin page</h3>
-            <label>Username</label>
-            <input value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})}/>
-            <label>Password</label>
-            <input value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
-            <button onClick={signin}> Signin </button>
-            <button onClick={cancel}> Cancel </button>
-
+  return (
+    <div className="backgroudStyle">
+      <div className="contentBox">
+        <div className="SignInStyle">
+          <label>Sign-In Page</label>
         </div>
-    );
+        <div
+          style={{
+            justifyContent: "center",
+            color: "#fec524",
+            alignItems: "center",
+            // display: "flex",
+            textAlign: "center",
+          }}
+        >
+          <div style={{ margin: "5px" }}>
+            <label>Username</label>
+            <input
+              value={credentials.username}
+              onChange={(e) =>
+                setCredentials({ ...credentials, username: e.target.value })
+              }
+            />
+          </div>
+          <div style={{ margin: "5px" }}>
+            <label>Password</label>
+            <input
+              value={credentials.password}
+              onChange={(e) =>
+                setCredentials({ ...credentials, password: e.target.value })
+              }
+            />
+          </div>
+          <button className="buttonStyle" onClick={signin}>
+            {" "}
+            Signin{" "}
+          </button>
+          <button className="buttonStyle" onClick={cancel}>
+            {" "}
+            Cancel{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Signin;
